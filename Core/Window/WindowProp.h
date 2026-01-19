37af6c4814712d91b37e66c8bead98f78b87186c
AayushBade14 <aayushrajubade@gmail.com>
2026-01-19 22:35:58 +0530

    Added Core/Window/WindowProp.h: A structure which holds all the window properties

---
+++ Core/Window/WindowProp.h
@@ -0,0 +1,20 @@
+//-----------------------------------------------------------------------------
+//  
+//  Name: WindowProp.h
+//
+//  Desc: Defines a structure which holds the window properties
+//
+//  Author: Aayush Bade 2026 (aayushbade14.github.io/Portfolio)
+//
+//-----------------------------------------------------------------------------
+
+#pragma once
+
+#include <string>
+
+struct WindowProp
+{
+  int Width;
+  int Height;
+  std::string Title;
+};
