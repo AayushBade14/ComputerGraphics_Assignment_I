c6e90747a6af881c52d56d12bd83e7a1dabd657b
AayushBade14 <aayushrajubade@gmail.com>
2026-01-20 00:57:27 +0530

    Added Core/Event/Event.h: This header defines all kinds of events and the data that the event must carry to perform necessary operations

--- Core/Event/Event.h
+++ Core/Event/Event.h
@@ -17,4 +17,86 @@ struct Event
   EventType type;
 };
 
- 
+struct KeyPressedEvent : Event
+{
+  KeyCode key;
+  bool isHeld;
+
+  KeyPressedEvent(KeyCode k, bool r)
+  {
+    type = EventType::KeyPressed;
+    key = k;
+    isHeld = r;
+  }
+};
+
+struct KeyReleasedEvent : Event
+{
+  KeyCode key;
+
+  KeyReleasedEvent(KeyCode k)
+  {
+    type = EventType::KeyReleased;
+    key = k;
+  }
+};
+
+struct MouseMovedEvent : Event
+{
+  float x;
+  float y;
+  float dx;
+  float dy;
+
+  MouseMoved(float x, float y, float dx, float dy)
+  {
+    type = EventType::MouseMoved;
+    this->x = x;
+    this->y = y;
+    this->dx = dx;
+    this->dy = dy;
+  }
+};
+
+struct MouseButtonPressedEvent : Event
+{
+  MouseButton button;
+
+  MouseButtonPressedEvent(MouseButton btn)
+  {
+    type = EventType::MouseButtonPressed;
+    button = btn;
+  }
+};
+
+struct MouseButtonReleasedEvent : Event
+{
+  MouseButton button;
+
+  MouseButtonReleasedEvent(MouseButton btn)
+  {
+    type = EventType::MouseButtonReleased;
+    button = btn;
+  }
+};
+
+struct WindowResizeEvent : Event
+{
+  int width;
+  int height;
+
+  WindowResizeEvent(int w, int h)
+  {
+    type = EventType::WindowResize;
+    width = w;
+    height = h;
+  }
+};
+
+struct WindowCloseEvent : Event
+{
+  WindowCloseEvent()
+  {
+    type = EventType::WindowClose;
+  }
+};
