2186f8a4cf69520001c6d6708788286116022636
AayushBade14 <aayushrajubade@gmail.com>
2026-01-19 23:12:29 +0530

    Added App/Application.h: Declared the application class

---
+++ App/Application.h
@@ -0,0 +1,19 @@
+//-----------------------------------------------------------------------------
+//  
+//  Name: Application.h
+//
+//  Desc: Header for the application class - the main application instance
+//
+//  Author: Aayush Bade 2026 (aayushbade14.github.io/Portfolio)
+//
+//-----------------------------------------------------------------------------
+
+#pragma once
+
+class Application
+{
+  public:
+
+    void Run();
+
+};
