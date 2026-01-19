22fc298308af52b850158919eb52d96166cd1dd5
AayushBade14 <aayushrajubade@gmail.com>
2026-01-19 22:38:25 +0530

    Added Core/Backend/Backend.h: Defines a enum class for specifying backend to be used for window and input systems

---
+++ Core/Backend/Backend.h
@@ -0,0 +1,19 @@
+//-----------------------------------------------------------------------------
+//  
+//  Name: Backend.h
+//
+//  Desc: Defines enum class for which backend to use for window/input systems
+//
+//  Author: Aayush Bade 2026 (aayushbade14.github.io/Portfolio)
+//
+//-----------------------------------------------------------------------------
+
+#pragma once
+
+enum class Backend
+{
+  GLFW,
+  SDL,
+  UNKNOWN
+};
+
