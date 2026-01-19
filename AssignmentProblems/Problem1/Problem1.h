eaf55f813d440558a85d69972e486dd9e0f2caed
AayushBade14 <aayushrajubade@gmail.com>
2026-01-19 23:20:30 +0530

    Added AssignmentProblems/Problem1/Problem1.h: Header for solution of problem1

---
+++ AssignmentProblems/Problem1/Problem1.h
@@ -0,0 +1,28 @@
+//-----------------------------------------------------------------------------
+//  
+//  Name: Problem1.h
+//
+//  Desc: Implements the solution of problem-1
+//
+//  Author: Aayush Bade 2026 (aayushbade14.github.io/Portfolio)
+//
+//-----------------------------------------------------------------------------
+
+#pragma once
+
+#include "../AssignmentProblems.h"
+
+class Problem1 : public AssignmentProblems
+{
+  
+  public:
+
+    Problem1() = default;
+
+    ~Problem1() = default;
+
+    void Run() override;
+
+};
+
+
