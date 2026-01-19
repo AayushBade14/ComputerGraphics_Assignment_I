b152b4334b46bf4ce96e4be3f53611ff77412ff6
AayushBade14 <aayushrajubade@gmail.com>
2026-01-19 23:24:04 +0530

    Added AssignmentProblems/Problem1/Problem1.cpp: Not yet the full solution, but just for testing the window abstraction made sofar

---
+++ AssignmentProblems/Problem1/Problem1.cpp
@@ -0,0 +1,30 @@
+//-----------------------------------------------------------------------------
+//  
+//  Name: Problem1.h
+//
+//  Desc: Implements the solution of problem-1
+//
+//  Author: Aayush Bade 2026 (aayushbade14.github.io/Portfolio)
+//
+//-----------------------------------------------------------------------------
+
+#include "./Problem1.h"
+
+#include "../../Core/WindowFactory/WindowFactory.h"
+
+void Problem1::Run()
+{
+
+  WindowProp prop;
+  prop.Width = 1280;
+  prop.Height = 720;
+  prop.Title = "Problem1 - B23CS1084"
+
+  std::unique_ptr<Window> window = WindowFactory::Create(Backend::GLFW, prop);
+
+  while(!window->ShouldClose())
+  {
+    window->SwapBuffers();
+  }
+
+}
