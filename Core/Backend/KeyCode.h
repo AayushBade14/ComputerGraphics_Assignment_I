b46b7e4b2350ad196e24444066bd002acd64a806
AayushBade14 <aayushrajubade@gmail.com>
2026-01-20 00:28:53 +0530

    Added Core/Backend/KeyCode.h: Defines all the keycodes for the engine

---
+++ Core/Backend/KeyCode.h
@@ -0,0 +1,36 @@
+//-----------------------------------------------------------------------------
+//  
+//  Name: KeyCode.h
+//
+//  Desc: Defines enum class for KeyCodes
+//
+//  Author: Aayush Bade 2026 (aayushbade14.github.io/Portfolio)
+//
+//-----------------------------------------------------------------------------
+
+enum class KeyCode
+{
+  Unknown = 0,
+
+  A, B, C, D, E, F, G, H, I,
+  J, K, L, M, N, O, P, Q, R,
+  S, T, U, V, W, X, Y, Z,
+
+  SPACE,
+  ESCAPE,
+  ENTER,
+  TAB,
+  BACKSPACE,
+
+  LEFT, RIGHT, UP, DOWN,
+
+  LSHIFT, RSHIFT,
+  LCTRL, RCTRL,
+  LALT, RALT,
+
+  N1, N2, N3, N4, N5,
+  N6, N7, N8, N9, N0,
+
+  F1, F2, F3, F4, F5, F6,
+  F7, F8, F9, F10, F11, F12
+};
---
+++ Core/Event/Event.h
@@ -0,0 +1,20 @@
+//-----------------------------------------------------------------------------
+//  
+//  Name: Event.h
+//
+//  Desc: Declares a struct for the Event from which other events can inherit
+//
+//  Author: Aayush Bade 2026 (aayushbade14.github.io/Portfolio)
+//
+//-----------------------------------------------------------------------------
+
+#pragma once
+
+#include "./EventType.h"
+
+struct Event
+{
+  EventType type;
+};
+
+ 
