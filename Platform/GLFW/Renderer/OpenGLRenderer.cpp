7bd038a00e6f62559faf919ae1064f65b943283e
AayushBade14 <aayushrajubade@gmail.com>
2026-01-20 21:02:13 +0530

    Added Platform/GLFW/Renderer/OpenGLRenderer.cpp: ogl renderer which implements the renderer interface

---
+++ Platform/GLFW/Renderer/OpenGLRenderer.cpp
@@ -0,0 +1,45 @@
+//-----------------------------------------------------------------------------
+//  
+//  Name: OpenGLRenderer.cpp
+//
+//  Desc: Implements the renderer interface using OpenGL as it's backend
+//
+//  Author: Aayush Bade 2026 (aayushbade14.github.io/Portfolio)
+//
+//-----------------------------------------------------------------------------
+
+#include "./OpenGLRenderer.h"
+#include <glad/glad.h>
+#include <iostream>
+#include <cstdlib>
+
+void OpenGLRenderer::Init()
+{
+  if(!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+  {
+    std::cerr << "ERROR: GLAD::Init()" << std::endl;
+    exit(1);
+  }
+
+  std::cout << "OpenGL initialized!" << std::endl;
+}
+
+void OpenGLRenderer::Shutdown()
+{
+
+}
+
+void OpenGLRenderer::BeginFrame()
+{
+  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+}
+
+void OpenGLRenderer::EndFrame()
+{
+
+}
+
+void OpenGLRenderer::SetViewport(int x, int y, int w, int h)
+{
+  glViewport(x, y, w, h);
+}
